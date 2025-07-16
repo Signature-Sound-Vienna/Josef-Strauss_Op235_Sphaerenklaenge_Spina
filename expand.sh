e0fe93775c01c3d5a2563488df0bcf553a64de92
Werner Goebl <goebl@mdw.ac.at>
2025-07-16 15:42:03 +0200

    Expanded versions updated

--- expand.sh
+++ expand.sh
@@ -1,5 +1,5 @@
-verovio --expand 'Version_Krauss1951' -a -t mei -o Version_Krauss1951.mei Sphaerenklaenge_Spina.mei
-verovio --expand 'Version_Krauss1951' -a -t timemap -o Version_Krauss1951-timemap.json Sphaerenklaenge_Spina.mei
+verovio --expand 'Longest-Version_Krauss1951' -a -t mei -o Longest-Version_Krauss1951.mei Sphaerenklaenge_Spina.mei
+verovio --expand 'Longest-Version_Krauss1951' -a -t timemap -o Longest-Version_Krauss1951-timemap.json Sphaerenklaenge_Spina.mei
 verovio --expand 'Short-Version_Boskovsky1964' -a -t mei -o Short-Version_Boskovsky1964.mei Sphaerenklaenge_Spina.mei
 verovio --expand 'Shortest-Version_Karajan1971' -a -t mei -o Shortest-Version_Karajan1971.mei Sphaerenklaenge_Spina.mei
 verovio --expand 'Version_BauerTheussl1985' -a -t mei -o Version_BauerTheussl1985.mei Sphaerenklaenge_Spina.mei
