05c5c2c68c79846f38531a36ff6413693eed0f0c
Werner Goebl <goebl@mdw.ac.at>
2025-07-17 14:24:13 +0200

    Expanded versions updated

--- expand.sh
+++ expand.sh
@@ -1,6 +1,6 @@
-verovio --expand 'Longest-Version_Krauss1951' -a -t mei -o Longest-Version_Krauss1951.mei Sphaerenklaenge_Spina.mei
-verovio --expand 'Longest-Version_Krauss1951' -a -t timemap -o Longest-Version_Krauss1951-timemap.json Sphaerenklaenge_Spina.mei
-verovio --expand 'Short-Version_Boskovsky1964' -a -t mei -o Short-Version_Boskovsky1964.mei Sphaerenklaenge_Spina.mei
-verovio --expand 'Shortest-Version_Karajan1971' -a -t mei -o Shortest-Version_Karajan1971.mei Sphaerenklaenge_Spina.mei
-verovio --expand 'Version_BauerTheussl1985' -a -t mei -o Version_BauerTheussl1985.mei Sphaerenklaenge_Spina.mei
+verovio --expand 'Longest-Version_Krauss1951' -a -t mei -o Longest-Version_Krauss1951.mei Josef-Strauss_Sphaerenklaenge_Spina.mei
+verovio --expand 'Longest-Version_Krauss1951' -a -t timemap -o Longest-Version_Krauss1951-timemap.json Josef-Strauss_Sphaerenklaenge_Spina.mei
+verovio --expand 'Short-Version_Boskovsky1964' -a -t mei -o Short-Version_Boskovsky1964.mei Josef-Strauss_Sphaerenklaenge_Spina.mei
+verovio --expand 'Shortest-Version_Karajan1971' -a -t mei -o Shortest-Version_Karajan1971.mei Josef-Strauss_Sphaerenklaenge_Spina.mei
+verovio --expand 'Version_BauerTheussl1985' -a -t mei -o Version_BauerTheussl1985.mei Josef-Strauss_Sphaerenklaenge_Spina.mei
 
